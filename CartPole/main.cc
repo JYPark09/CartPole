ba3c8dbdfe816aee47929c0586f100cf6dd14a50
JYPark09 <jyp10987@gmail.com>
2021-05-19 18:57:53 +0900

    feat: implement RL training code

--- CartPole/main.cc
+++ CartPole/main.cc
@@ -1,37 +1,79 @@
+#include <deque>
 #include <iostream>
 
+#include "Agent.hpp"
 #include "CartPole.hpp"
 #include "CartPoleRenderer.hpp"
 
 using std::cout;
 using std::endl;
 
+struct EpisodeResult final
+{
+    int steps{ 0 };
+    float reward{ 0 };
+};
+
+//! Returns total reward
+EpisodeResult ProcEpisode(Agent& agent, Renderer& renderer, bool render);
+
 int main()
 {
+    Agent agent;
     Renderer renderer;
     renderer.Create();
+
+    std::deque<float> results;
+
+    agent.SetGamma(0.99f);
+    agent.SetLearningRate(1e-3f);
+
+    for (int ep = 1; ; ++ep)
+    {
+        const auto result = ProcEpisode(agent, renderer, (ep % 10 == 0));
+
+        cout << "Episode " << ep << " - total reward: " << result.reward << endl;
+    }
+
+    renderer.Close();
+}
+
+EpisodeResult ProcEpisode(Agent& agent, Renderer& renderer, bool render)
+{
+    EpisodeResult result;
+
+    agent.Clear();
 
     CartPole::Env env;
 
     CartPole::State state = env.Reset();
-    while (true)
+    for (result.steps = 0; result.steps < 500; ++result.steps)
     {
-        renderer.Render(state);
+        if (render)
+            renderer.Render(state);
 
-        auto [newState, reward, done] = env.Step(CartPole::Action::RIGHT);
+        CartPole::Action action;
+        tiny_dnn::vec_t pred;
+        std::tie(action, pred) = agent.GetAction(state);
 
-        cout << "x: " << state.Desc.x << " d_x: " << state.Desc.d_x
-             << " theta: " << state.Desc.theta
-             << " d_theta: " << state.Desc.d_theta << " reward: " << reward
-             << endl;
+        CartPole::State newState;
+        float reward;
+        bool done;
+        std::tie(newState, reward, done) = env.Step(action);
+        result.reward += reward;
+
+        if (done)
+            reward = -100;
+
+        agent.AddInterActionInfo(state, action, pred, reward);
 
         state = newState;
 
         if (done)
             break;
     }
-    while (true)
-        ;
 
-    renderer.Close();
+    agent.Train();
+
+    return result;
 }
